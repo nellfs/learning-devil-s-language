4d77b5706d010f6a66cab33754ab485265d0374c
nellfs <pokycelit@gmail.com>
2023-12-27 10:08:40 -0300

    best practice for local variables

--- 002.cpp
+++ 002.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-int main()
-{
-    int firstNumber{};
-    int secondNumber{};
+int main() {
+  std::cout << "Enter a integer: ";
+  int firstNumber{};
+  std::cin >> firstNumber;
 
-    std::cout << "Enter a integer: ";
-    std::cin >> firstNumber;
-    std::cout << "Enter another integer: ";
-    std::cin >> secondNumber;
-    std::cout << firstNumber << " + " << secondNumber << " is " << firstNumber + secondNumber << ".\n";
-    std::cout << firstNumber << " - " << secondNumber << " is " << firstNumber - secondNumber << ".\n";
+  std::cout << "Enter another integer: ";
+  int secondNumber{};
+  std::cin >> secondNumber;
+
+  std::cout << firstNumber << " + " << secondNumber << " is "
+            << firstNumber + secondNumber << ".\n";
+  std::cout << firstNumber << " - " << secondNumber << " is "
+            << firstNumber - secondNumber << ".\n";
 }
