fcc3f792ece71c670e9cd933cc3b81e31e399a4d
nellfs <pokycelit@gmail.com>
2023-12-27 10:29:32 -0300

    forward declaration: function prototype

---
+++ 005.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// forward declaration
+int add(int x, int y); // function prototype
+
+int main() {
+  std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
+  return 0;
+}
+
+int add(int x, int y) { return x + y; }
