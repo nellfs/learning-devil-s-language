d2b6b8b90a78d0b624aa383f6f90fbc8be7ea358
nellfs <pokycelit@gmail.com>
2024-01-01 11:27:58 -0300

    sizeof operator

--- 008.cpp
+++ 008.cpp
@@ -1,9 +1,16 @@
+#include <iomanip>
 #include <iostream>
 
-void printValue(int value) { std::cout << value << '\n'; }
-
 int main() {
-  printValue(5);
-
-  return 0;
+  std::cout << std::left;
+  std::cout << std::setw(16) << "bool: " << sizeof(bool) << " bytes\n";
+  std::cout << std::setw(16) << "char:" << sizeof(char) << " bytes\n";
+  std::cout << std::setw(16) << "short:" << sizeof(short) << " bytes\n";
+  std::cout << std::setw(16) << "int:" << sizeof(int) << " bytes\n";
+  std::cout << std::setw(16) << "long:" << sizeof(long) << " bytes\n";
+  std::cout << std::setw(16) << "long long:" << sizeof(long long) << " bytes\n";
+  std::cout << std::setw(16) << "float:" << sizeof(float) << " bytes\n";
+  std::cout << std::setw(16) << "double:" << sizeof(double) << " bytes\n";
+  std::cout << std::setw(16) << "long double:" << sizeof(long double)
+            << " bytes\n";
 }
